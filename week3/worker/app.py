1330b5e5e5bf8d778cbdeefec28d425af6339760
gaeng02 <sondin144@gmail.com>
2025-09-16 16:28:05 +0900

    week3 worker

--- week3/worker/app.py
+++ week3/worker/app.py
@@ -9,30 +9,72 @@ LOG_DIR.mkdir(parents = True, exist_ok = True)
 LOG_FILE = LOG_DIR / "events.log"
 
 def write_log (event : dict) :
-    event["logged_at"] = datetime.now()
+    event["logged_at"] = datetime.now().isoformat(timespec="seconds")
     with open(LOG_FILE, "a", encoding="utf-8") as f :
         f.write(json.dumps(event, ensure_ascii = False) + "\n")
 
 @app.post("/login")
 async def login (request : Request) :
     data = await request.json()
-    write_log({"endpoint": "login", **data})
+    
+    # 수정 가능
+    event = {
+        "endpoint": "login",
+        "ts": data.get("ts"),
+        "user": data.get("user"),
+        "password": data.get("password"),
+        "action": data.get("action")
+    }
+    
+    print("[WORKER] Received login:", event)
+    write_log(event)
     return {"status": "ok"}
 
 @app.post("/main")
 async def main (request : Request) :
     data = await request.json()
-    write_log({"endpoint": "main", **data})
+    
+    # 수정 가능
+    event = {
+        "endpoint": "main",
+        "ts": data.get("ts"),
+        "user": data.get("user"),
+        "action": data.get("action")
+    }
+    
+    print("[WORKER] Received main:", event)
+    write_log(event)
     return {"status": "ok"}
 
 @app.post("/home")
 async def home (request: Request) :
     data = await request.json()
-    write_log({"endpoint": "home", **data})
+    
+    # 수정 가능
+    event = {
+        "endpoint": "home",
+        "ts": data.get("ts"),
+        "user": data.get("user"),
+        "action": data.get("action")
+    }
+    
+    print("[WORKER] Received home:", event)
+    write_log(event)
     return {"status": "ok"}
 
 @app.post("/post/{post_id}")
 async def post (post_id : int, request : Request) :
     data = await request.json()
-    write_log({"endpoint": f"post_{post_id}", **data})
+    
+    # 수정 가능
+    event = {
+        "endpoint": f"post_{post_id}",
+        "ts": data.get("ts"),
+        "user": data.get("user"),
+        "action": data.get("action"),
+        "post_id": data.get("post_id")
+    }
+    
+    print("[WORKER] Received post:", event)
+    write_log(event)
     return {"status": "ok"}
